2e5886662c7cf240219622da8931d319b53ffdb2
Jorn <19970839+ThorsAvenger@user.noreply.github.com>
2024-01-21 13:41:14 +0100

    Added IMU sensor publisher back in

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,10 @@
 // #include <geometry_msgs/msg/twist.h>
 #include <nav_msgs/msg/odometry.h>
 
+#include <sensor_msgs/msg/imu.h>
+#include <sensor_msgs/msg/magnetic_field.h>
+#include <sensor_msgs/msg/temperature.h>
+
 #include <ICM_20948.h>
 #include <TMC2209.h>
 
@@ -19,6 +23,15 @@
 //publisher
 rcl_publisher_t publisher;
 std_msgs__msg__Int32 msg_pub;
+
+rcl_publisher_t publisher_imu;
+sensor_msgs__msg__Imu msg_imu;
+
+rcl_publisher_t publisher_mag;
+sensor_msgs__msg__MagneticField msg_mag;
+
+rcl_publisher_t publisher_temp;
+sensor_msgs__msg__Temperature msg_temp;
 
 // subscriber
 rcl_subscription_t subscriber;
@@ -31,9 +44,12 @@ rclc_support_t support;
 rcl_allocator_t allocator;
 rclc_executor_t executor;
 rcl_timer_t timer;
-unsigned int num_handles = 2;   // 1 subscriber, 1 publisher
+unsigned int num_handles = 5;   // 1 subscriber, 1 publisher
 
 #define LED_PIN 13
+#define WIRE_PORT Wire
+#define AD0_VAL 1
+ICM_20948_I2C myICM; 
 
 #define RCCHECK(fn){rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){error_loop();}}
 #define RCSOFTCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){}}
@@ -49,8 +65,27 @@ void error_loop() {
 void timer_callback(rcl_timer_t * timer, int64_t last_call_time) {
   RCLC_UNUSED(last_call_time);
   if (timer != NULL) {
-    RCSOFTCHECK(rcl_publish(&publisher, &msg_pub, NULL));
-    msg_pub.data++;
+    if(myICM.dataReady()) { 
+      myICM.getAGMT(); 
+       
+      
+    }
+
+    msg_imu.linear_acceleration.x = myICM.accX();
+    msg_imu.linear_acceleration.y = myICM.accY();
+    msg_imu.linear_acceleration.z = myICM.accZ();
+    msg_imu.angular_velocity.x = myICM.gyrX();
+    msg_imu.angular_velocity.y = myICM.gyrY();
+    msg_imu.angular_velocity.z = myICM.gyrZ();
+    msg_mag.magnetic_field.x = myICM.magX();
+    msg_mag.magnetic_field.y = myICM.magY();
+    msg_mag.magnetic_field.z = myICM.magZ();
+    msg_temp.temperature = myICM.temp();
+    RCSOFTCHECK(rcl_publish(&publisher_temp, &msg_temp, NULL));
+    RCSOFTCHECK(rcl_publish(&publisher_imu, &msg_imu, NULL));
+    RCSOFTCHECK(rcl_publish(&publisher_mag, &msg_mag, NULL));
+
+    //msg.data++;
   }
 }
 
@@ -98,9 +133,27 @@ void setup() {
 
   // create publisher
   RCCHECK(rclc_publisher_init_default(
+    &publisher_imu,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, Imu),
+    "micro_ros_platformio_node_publisher_imu"));
+
+   RCCHECK(rclc_publisher_init_default(
+    &publisher_mag,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, MagneticField),
+    "micro_ros_platformio_node_publisher_mag"));   
+
+    RCCHECK(rclc_publisher_init_default(
+    &publisher_temp,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, Temperature),
+    "micro_ros_platformio_node_publisher_temp"));   
+
+  RCCHECK(rclc_publisher_init_default(
     &publisher,
     &node,
-    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Float32),
     "micro_ros_platformio_node_publisher"));
 
   // create subscriber
@@ -111,7 +164,7 @@ void setup() {
     "odom"));
 
   // create timer,
-  const unsigned int timer_timeout = 1000;
+  const unsigned int timer_timeout = 100;
   RCCHECK(rclc_timer_init_default(
     &timer,
     &support,
@@ -124,10 +177,13 @@ void setup() {
   RCCHECK(rclc_executor_add_subscription(&executor, &subscriber, &msg_sub, &subscription_callback, ON_NEW_DATA));   
 
   msg_pub.data = 0;
+   WIRE_PORT.begin();
+  WIRE_PORT.setClock(400000);
+  myICM.begin(WIRE_PORT, AD0_VAL);
   Serial.println("Finished initialization.");
 }
 
 void loop() {
-  delay(10  );
-  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(10)));
+  delay(100);
+  RCSOFTCHECK(rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100)));
 }
